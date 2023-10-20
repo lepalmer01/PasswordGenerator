3762dc0ac6db84123163e97ba1b61fd344109114
LE Palmer <lpalmer@ucsd.edu>
2023-10-20 12:02:06 -0700

    added window prompt for user to choose character count/added lowercase prompt with if statement

--- script.js
+++ script.js
@@ -2,19 +2,37 @@
 var generateBtn = document.querySelector("#generate");
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChars = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ];
+var specialChars = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var userChoice = 0
 var choicesArray = []
 
 function generatePassword() {
-  console.log ("clicked");
-  while(userCHoice < 8 || userCHoice > 128 || isNaN(userChoice) || userChoice === "") {
+  console.log("clicked");
+  while (userCHoice < 8 || userCHoice > 128 || isNaN(userChoice) || userChoice === "") {
+    userChoice = window.prompt("How many characters would you like your password to contain?")
+  }
+
+  var lowerCasechoice = window.confirm ("Would you like lowercase letters in your password?")
+  if (lowerCasechoice) {
+    choicesArray = choicesArray.concat (lowercase)
+  }
 
 
-  }
-  }
-  
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
